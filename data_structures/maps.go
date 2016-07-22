a081d93c16b820972d9a7255b52de8a2933667fc
Shyam Sathyanathan <shyamsathyanathan@yahoo.in>
2016-07-22 18:01:32 +0530

    Completed maps.go

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,22 @@
 package data_structures
 
 func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+	result := []string {}
+	for key, value := range data {
+		for _, itst := range value {
+			if itst == interest {
+				result = append(result, key)
+			}
+		}
+	}
+	return result
 }
 
 func contains(src []string, elem string) bool {
+	for _, value := range src {
+		if value == elem {
+			return true
+		}
+	}
 	return false
 }
