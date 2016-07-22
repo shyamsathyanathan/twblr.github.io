a114510da0ecd75a20012ad6dc51263811b0ad49
Shyam Sathyanathan <shyamsathyanathan@yahoo.in>
2016-07-22 16:07:49 +0530

    Completed for_structure.go recursively

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,16 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	count := 0
+	if n == 1 {
+		return count;
+	} else if n % 2 == 0 {
+		count++
+		count := count + collatzChainLength(n / 2)
+		return count
+	} else {
+		count++
+		count := count + collatzChainLength(3 * n + 1)
+		return count
+	}
 }
