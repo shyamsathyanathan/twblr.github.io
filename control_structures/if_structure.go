2bf567d62c4dedc6df01278b55c42976cc1acbe8
Shyam Sathyanathan <shyamsathyanathan@yahoo.in>
2016-07-22 15:28:43 +0530

    Completed if_condition_test

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,14 @@
 package control_structures
 
+import "fmt"
+
 func fizzBuzz(i int32) string {
-	return ""
+	if (i % 3) == 0 && (i % 5) == 0 {
+		return "FizzBuzz"
+	} else if (i % 3) == 0 {
+		return "Fizz"
+	} else if (i % 5) == 0 {
+		return "Buzz"
+	}
+	return fmt.Sprintf("%d", i)
 }
