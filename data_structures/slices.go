f2e7a7330b6ade038432e706e74fb3ae09c70a20
Shyam Sathyanathan <shyamsathyanathan@yahoo.in>
2016-07-22 17:47:45 +0530

    Partially completed slices.go

--- data_structures/slices.go
+++ data_structures/slices.go
@@ -4,16 +4,28 @@ type mapOperation func(int32) int32
 type filterOperation func(int32) bool
 
 func mapInts(op mapOperation, vals []int32) []int32 {
-	return []int32{}
-
+	result := []int32{}
+	for _, value := range vals {
+		result = append(result, op(value))
+	}
+	return result
 }
 
 func filterInts(op filterOperation, vals []int32) []int32 {
-	return []int32{3}
+	result := []int32{}
+	for _, value := range vals {
+		if op(value) {
+			result = append(result, value)
+		}
+	}
+	return result
 }
 
 func concatenate(dest []string, newValues ...string) []string {
-	return nil
+	for _, value := range newValues {
+		dest = append(dest, value)
+	}
+	return dest
 }
 
 func equals(list1 []string, list2 []string) bool {
