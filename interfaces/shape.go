de2bd34f291c423576333f753fdafbc79ae037b2
Shyam Sathyanathan <shyamsathyanathan@yahoo.in>
2016-07-23 10:44:45 +0530

    Completed shape.go

--- interfaces/shape.go
+++ interfaces/shape.go
@@ -3,3 +3,28 @@ package interfaces
 type Shape interface {
 	Area() int
 }
+
+type Square struct {
+	side int
+}
+
+type Hybrid struct {
+	square Square
+	rectangle Rectangle
+}
+
+type Rectangle struct {
+	length, breadth int
+}
+
+func (s *Square) Area() int {
+	return s.side * s.side
+}
+
+func (r *Rectangle) Area() int {
+	return r.length * r.breadth
+}
+
+func (h *Hybrid) Area() int {
+	return h.square.Area() + h.rectangle.Area()
+}
